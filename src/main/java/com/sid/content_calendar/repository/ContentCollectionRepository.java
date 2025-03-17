ec97721dfcd777ad1a231026348d455ba83f32da
Siddhesh Khedekar <siddheshkhedekar.dev@gmail.com>
2025-03-17 21:07:36 +0100

    add dummy repository logic

--- src/main/java/com/sid/content_calendar/repository/ContentCollectionRepository.java
+++ src/main/java/com/sid/content_calendar/repository/ContentCollectionRepository.java
@@ -1,7 +1,23 @@
 package com.sid.content_calendar.repository;
 
+import com.sid.content_calendar.model.Content;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ContentCollectionRepository {
+
+    private final List<Content> content = new ArrayList<>();
+
+    public List<Content> findAll() {
+        return content;
+    }
+
+    public Optional<Content> findById(Integer id) {
+        return content.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+
 }
